eee312d53ebee5a9b36bafb534bf02dfed042c73
Daniel da Silva Moura <danielx1788@gmail.com>
2022-12-16 23:37:47 -0300

    comit

--- APIs/views.py
+++ APIs/views.py
@@ -1,20 +1,22 @@
 from django.core.mail import EmailMessage
 from django.shortcuts import render
 from rest_framework import viewsets
-
+from rest_framework.response import Response
 from .models import Consult
 from .serializers import EnvioSerializer
+from rest_framework import views, status, generics
+from django.core.mail import send_mail
 
 
-def send_email():
-    email = EmailMessage(
-        'Title',
-        (EnvioSerializer.para, EnvioSerializer.titulo, EnvioSerializer.texto),
-        'my-email',
-        ['my-receive-email']
-    )
-    email.attach_file(EnvioSerializer.file)
-    email.send()
+class Sender(views.APIView):
+    def get(self, request):
+        print(request.data)
+        email = {'to': 'joaorocha@email.com', 'title': 'Some title', 
+                 'message': 'Some message'}
+        serializer = EnvioSerializer(email)
+
+        return Response(serializer.data)
+
 
 
 class Envio_ViewSet(viewsets.ModelViewSet):
@@ -23,3 +25,13 @@ class Envio_ViewSet(viewsets.ModelViewSet):
 
 
 
+    send_mail(
+            serializer_class.data['title'],
+            serializer_class.data['message'],
+            from_email=None,
+            recipient_list=[serializer_class.data['to'],],
+            fail_silently=False,
+        )
+
+
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
